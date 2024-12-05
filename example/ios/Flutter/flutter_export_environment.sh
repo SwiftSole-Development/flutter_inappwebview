266a3ddd7f3e866383d47d9f9d23a72bc4e3f892
Owen Stelmarski <hypedsupremelife@gmail.com>
2024-12-05 08:05:11 -0600

    test

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/owenkealey/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/owenkealey/Projects/flutter_inappwebview/example"
+export "FLUTTER_ROOT=/Users/ostelmarski/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ostelmarski/Documents/GitHub/flutter_inappwebview/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/ostelmarski/Documents/GitHub/flutter_inappwebview/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=/Users/ostelmarski/Documents/GitHub/flutter_inappwebview/example/.dart_tool/package_config.json"
